e00d1b79c748f3f3035f0793989e5a4ad8312897
Laurence Emms <laurenceemms@gmail.com>
2024-05-05 12:50:17 -0700

    Added variadic array

--- src/main.cc
+++ src/main.cc
@@ -10,5 +10,29 @@
 int main(int argc, char** argv) {
     std::cout << "Variadic arrays" << std::endl;
 
+    var::Array<double, 1> one{0.25};
+
+    var::Array<double, 2> two{0.25, 0.5};
+
+    var::Array<double, 3> three{0.25, 0.5, 0.75};
+
+    std::cout << "One[0]: " << var::get<0>(one) << std::endl;
+
+    // The following is a compile error
+    // std::cout << "One[1]: " << var::get<1>(one) << std::endl;
+
+    std::cout << "Two[0]: " << var::get<0>(two) << std::endl;
+    std::cout << "Two[1]: " << var::get<1>(two) << std::endl;
+
+    // The following is a compile error
+    // std::cout << "Two[2]: " << var::get<2>(two) << std::endl;
+
+    std::cout << "Three[0]: " << var::get<0>(three) << std::endl;
+    std::cout << "Three[1]: " << var::get<1>(three) << std::endl;
+    std::cout << "Three[2]: " << var::get<2>(three) << std::endl;
+
+    // The following is a compile error
+    // std::cout << "Three[3]: " << var::get<3>(three) << std::endl;
+
     return 0;
 }
--- src/var_array.h
+++ src/var_array.h
@@ -1,2 +1,28 @@
 // Variadic arrays
 // Copyright Laurence Emms 2024
+
+
+namespace var {
+    template <typename T, int n>
+    struct Array : public Array<T, n - 1> {
+        template <typename ...Ts>
+        Array(T arg, Ts... args) : data(arg), Array<T, n - 1>(args...) {}
+
+        T data;
+    };
+
+    template <typename T>
+    struct Array<T, 0> {
+        Array() {}
+    };
+
+    template <int n, typename T>
+    T& get(Array<T, n + 1>& array) {
+        return array.data;
+    }
+
+    template <int n, typename T>
+    const T& get(const Array<T, n + 1>& array) {
+        return array.data;
+    }
+}
